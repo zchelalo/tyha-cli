454952056563a7e2d2ee4f77cd5a84317a78ee77
Eduardo Saavedra <eduardo.saavedra@ngxnetworks.com>
2025-03-03 13:58:58 -0700

    refactor: create module command

--- src/commands/create_module.ts
+++ src/commands/create_module.ts
@@ -121,47 +121,54 @@ export class CreateModule {
   }
 
   private static async modifyDomainFiles() {
-    // Replace placeholders in domain layer files
+    const entityPath = join(CreateModule.modulePath, 'domain/entity.ts')
+    const repositoryPath = join(CreateModule.modulePath, 'domain/repository.ts')
+    const valuePath = join(CreateModule.modulePath, 'domain/value.ts')
+
     // entity
-    await Files.replaceInFile(join(CreateModule.modulePath, 'domain/entity.ts'), '{{name}}', CreateModule.nameEntity)
-    await Files.replaceInFile(join(CreateModule.modulePath, 'domain/entity.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
+    await Files.replaceInFile(entityPath, '{{name}}', CreateModule.nameEntity)
+    await Files.replaceInFile(entityPath, '{{nameCamel}}', CreateModule.nameCamel)
 
     // repository
-    await Files.replaceInFile(join(CreateModule.modulePath, 'domain/repository.ts'), '{{name}}', CreateModule.nameEntity)
-    await Files.replaceInFile(join(CreateModule.modulePath, 'domain/repository.ts'), '{{CreateModule.nameClean}}', CreateModule.nameClean)
-    await Files.replaceInFile(join(CreateModule.modulePath, 'domain/repository.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
+    await Files.replaceInFile(repositoryPath, '{{name}}', CreateModule.nameEntity)
+    await Files.replaceInFile(repositoryPath, '{{nameClean}}', CreateModule.nameClean)
+    await Files.replaceInFile(repositoryPath, '{{nameCamel}}', CreateModule.nameCamel)
 
     // value
-    await Files.replaceInFile(join(CreateModule.modulePath, 'domain/value.ts'), '{{name}}', CreateModule.nameEntity)
-    await Files.replaceInFile(join(CreateModule.modulePath, 'domain/value.ts'), '{{CreateModule.nameClean}}', CreateModule.nameClean)
-    await Files.replaceInFile(join(CreateModule.modulePath, 'domain/value.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
+    await Files.replaceInFile(valuePath, '{{name}}', CreateModule.nameEntity)
+    await Files.replaceInFile(valuePath, '{{nameClean}}', CreateModule.nameClean)
+    await Files.replaceInFile(valuePath, '{{nameCamel}}', CreateModule.nameCamel)
   }
 
   private static async modifyApplicationFiles() {
-    // Replace placeholders in application layer files
-    // dtos
-    await Files.replaceInFile(join(CreateModule.modulePath, 'application/dtos/create.ts'), '{{name}}', CreateModule.nameEntity)
-    await Files.replaceInFile(join(CreateModule.modulePath, 'application/dtos/create.ts'), '{{CreateModule.nameClean}}', CreateModule.nameClean)
-    await Files.replaceInFile(join(CreateModule.modulePath, 'application/dtos/create.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
-    await Files.replaceInFile(join(CreateModule.modulePath, 'application/dtos/response.ts'), '{{name}}', CreateModule.nameEntity)
-    await Files.replaceInFile(join(CreateModule.modulePath, 'application/dtos/response.ts'), '{{CreateModule.nameClean}}', CreateModule.nameClean)
-    await Files.replaceInFile(join(CreateModule.modulePath, 'application/dtos/response.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
+    const dtoCreatePath = join(CreateModule.modulePath, 'application/dtos/create.ts')
+    const dtoResponsePath = join(CreateModule.modulePath, 'application/dtos/response.ts')
+    const schemaPath = join(CreateModule.modulePath, 'application/schemas/schema.ts')
+    const useCasesPath = join(CreateModule.modulePath, 'application/use_cases/use_cases.ts')
 
-    await Files.renameFile(join(CreateModule.modulePath, 'application/dtos/create.ts'), join(CreateModule.modulePath, `application/dtos/${CreateModule.nameClean}_create.ts`))
-    await Files.renameFile(join(CreateModule.modulePath, 'application/dtos/response.ts'), join(CreateModule.modulePath, `application/dtos/${CreateModule.nameClean}_response.ts`))
+    // dtos
+    await Files.replaceInFile(dtoCreatePath, '{{name}}', CreateModule.nameEntity)
+    await Files.replaceInFile(dtoCreatePath, '{{nameClean}}', CreateModule.nameClean)
+    await Files.replaceInFile(dtoCreatePath, '{{nameCamel}}', CreateModule.nameCamel)
+    await Files.replaceInFile(dtoResponsePath, '{{name}}', CreateModule.nameEntity)
+    await Files.replaceInFile(dtoResponsePath, '{{nameClean}}', CreateModule.nameClean)
+    await Files.replaceInFile(dtoResponsePath, '{{nameCamel}}', CreateModule.nameCamel)
+
+    await Files.renameFile(dtoCreatePath, join(CreateModule.modulePath, `application/dtos/${CreateModule.nameClean}_create.ts`))
+    await Files.renameFile(dtoResponsePath, join(CreateModule.modulePath, `application/dtos/${CreateModule.nameClean}_response.ts`))
 
     // schemas
-    await Files.replaceInFile(join(CreateModule.modulePath, 'application/schemas/schema.ts'), '{{name}}', CreateModule.nameEntity)
-    await Files.replaceInFile(join(CreateModule.modulePath, 'application/schemas/schema.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
+    await Files.replaceInFile(schemaPath, '{{name}}', CreateModule.nameEntity)
+    await Files.replaceInFile(schemaPath, '{{nameCamel}}', CreateModule.nameCamel)
 
-    await Files.renameFile(join(CreateModule.modulePath, 'application/schemas/schema.ts'), join(CreateModule.modulePath, `application/schemas/${CreateModule.nameClean}.ts`))
+    await Files.renameFile(schemaPath, join(CreateModule.modulePath, `application/schemas/${CreateModule.nameClean}.ts`))
 
     // use cases
-    await Files.replaceInFile(join(CreateModule.modulePath, 'application/use_cases/use_cases.ts'), '{{name}}', CreateModule.nameEntity)
-    await Files.replaceInFile(join(CreateModule.modulePath, 'application/use_cases/use_cases.ts'), '{{CreateModule.nameClean}}', CreateModule.nameClean)
-    await Files.replaceInFile(join(CreateModule.modulePath, 'application/use_cases/use_cases.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
+    await Files.replaceInFile(useCasesPath, '{{name}}', CreateModule.nameEntity)
+    await Files.replaceInFile(useCasesPath, '{{nameClean}}', CreateModule.nameClean)
+    await Files.replaceInFile(useCasesPath, '{{nameCamel}}', CreateModule.nameCamel)
 
-    await Files.renameFile(join(CreateModule.modulePath, 'application/use_cases/use_cases.ts'), join(CreateModule.modulePath, `application/use_cases/${CreateModule.nameClean}.ts`))
+    await Files.renameFile(useCasesPath, join(CreateModule.modulePath, `application/use_cases/${CreateModule.nameClean}.ts`))
   }
 
   private static async modifyInfrastructureFiles() {
@@ -183,40 +190,45 @@ export class CreateModule {
     ])
     const routerType = routerTypeAnswer.routerType
 
+    const restRouterPath = join(CreateModule.modulePath, 'infrastructure/rest.ts')
+    const restControllerPath = join(CreateModule.modulePath, 'infrastructure/rest_controller.ts')
+    const grpcRouterPath = join(CreateModule.modulePath, 'infrastructure/grpc.ts')
+    const grpcControllerPath = join(CreateModule.modulePath, 'infrastructure/grpc_controller.ts')
+
     switch (routerType) {
       case RouterType.REST:
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/rest.ts'), '{{name}}', CreateModule.nameEntity)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/rest.ts'), '{{CreateModule.nameClean}}', CreateModule.nameClean)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/rest.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
+        await Files.replaceInFile(restRouterPath, '{{name}}', CreateModule.nameEntity)
+        await Files.replaceInFile(restRouterPath, '{{nameClean}}', CreateModule.nameClean)
+        await Files.replaceInFile(restRouterPath, '{{nameCamel}}', CreateModule.nameCamel)
 
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/rest_controller.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/rest_controller.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/rest_controller.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
+        await Files.replaceInFile(restControllerPath, '{{nameCamel}}', CreateModule.nameCamel)
+        await Files.replaceInFile(restControllerPath, '{{nameCamel}}', CreateModule.nameCamel)
+        await Files.replaceInFile(restControllerPath, '{{nameCamel}}', CreateModule.nameCamel)
 
-        await Files.renameFile(join(CreateModule.modulePath, 'infrastructure/rest_controller.ts'), join(CreateModule.modulePath, 'infrastructure/controller.ts'))
+        await Files.renameFile(restControllerPath, join(CreateModule.modulePath, 'infrastructure/controller.ts'))
 
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/grpc.ts'))
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/grpc_controller.ts'))
+        await Files.deleteFile(grpcRouterPath)
+        await Files.deleteFile(grpcControllerPath)
         break;
       case RouterType.REST:
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/grpc.ts'), '{{name}}', CreateModule.nameEntity)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/grpc.ts'), '{{CreateModule.nameClean}}', CreateModule.nameClean)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/grpc.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
+        await Files.replaceInFile(grpcRouterPath, '{{name}}', CreateModule.nameEntity)
+        await Files.replaceInFile(grpcRouterPath, '{{nameClean}}', CreateModule.nameClean)
+        await Files.replaceInFile(grpcRouterPath, '{{nameCamel}}', CreateModule.nameCamel)
 
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/grpc_controller.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/grpc_controller.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/grpc_controller.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
+        await Files.replaceInFile(grpcControllerPath, '{{nameCamel}}', CreateModule.nameCamel)
+        await Files.replaceInFile(grpcControllerPath, '{{nameCamel}}', CreateModule.nameCamel)
+        await Files.replaceInFile(grpcControllerPath, '{{nameCamel}}', CreateModule.nameCamel)
 
-        await Files.renameFile(join(CreateModule.modulePath, 'infrastructure/grpc_controller.ts'), join(CreateModule.modulePath, 'infrastructure/controller.ts'))
+        await Files.renameFile(grpcControllerPath, join(CreateModule.modulePath, 'infrastructure/controller.ts'))
 
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/rest.ts'))
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/rest_controller.ts'))
+        await Files.deleteFile(restRouterPath)
+        await Files.deleteFile(restControllerPath)
         break;
       default:
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/grpc.ts'))
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/grpc_controller.ts'))
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/rest.ts'))
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/rest_controller.ts'))
+        await Files.deleteFile(grpcRouterPath)
+        await Files.deleteFile(grpcControllerPath)
+        await Files.deleteFile(restRouterPath)
+        await Files.deleteFile(restControllerPath)
         break;
     }
   }
@@ -236,35 +248,39 @@ export class CreateModule {
     ])
     const repositoryType = repositoryTypeAnswer.repositoryType
 
+    const drizzleRepositoryPath = join(CreateModule.modulePath, 'infrastructure/repositories/drizzle.ts')
+    const memoryRepositoryPath = join(CreateModule.modulePath, 'infrastructure/repositories/memory.ts')
+    const grpcRepositoryPath = join(CreateModule.modulePath, 'infrastructure/repositories/grpc.ts')
+
     switch (repositoryType) {
       case RepositoryType.DRIZZLE:
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/repositories/drizzle.ts'), '{{name}}', CreateModule.nameEntity)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/repositories/drizzle.ts'), '{{CreateModule.nameClean}}', CreateModule.nameClean)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/repositories/drizzle.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
+        await Files.replaceInFile(drizzleRepositoryPath, '{{name}}', CreateModule.nameEntity)
+        await Files.replaceInFile(drizzleRepositoryPath, '{{nameClean}}', CreateModule.nameClean)
+        await Files.replaceInFile(drizzleRepositoryPath, '{{nameCamel}}', CreateModule.nameCamel)
 
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/repositories/grpc.ts'))
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/repositories/memory.ts'))
+        await Files.deleteFile(grpcRepositoryPath)
+        await Files.deleteFile(memoryRepositoryPath)
         break;
       case RepositoryType.IN_MEMORY:
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/repositories/memory.ts'), '{{name}}', CreateModule.nameEntity)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/repositories/memory.ts'), '{{CreateModule.nameClean}}', CreateModule.nameClean)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/repositories/memory.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
+        await Files.replaceInFile(memoryRepositoryPath, '{{name}}', CreateModule.nameEntity)
+        await Files.replaceInFile(memoryRepositoryPath, '{{nameClean}}', CreateModule.nameClean)
+        await Files.replaceInFile(memoryRepositoryPath, '{{nameCamel}}', CreateModule.nameCamel)
 
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/repositories/grpc.ts'))
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/repositories/drizzle.ts'))
+        await Files.deleteFile(grpcRepositoryPath)
+        await Files.deleteFile(drizzleRepositoryPath)
         break;
       case RepositoryType.GRPC_CLIENT:
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/repositories/grpc.ts'), '{{name}}', CreateModule.nameEntity)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/repositories/grpc.ts'), '{{CreateModule.nameClean}}', CreateModule.nameClean)
-        await Files.replaceInFile(join(CreateModule.modulePath, 'infrastructure/repositories/grpc.ts'), '{{CreateModule.nameCamel}}', CreateModule.nameCamel)
+        await Files.replaceInFile(grpcRepositoryPath, '{{name}}', CreateModule.nameEntity)
+        await Files.replaceInFile(grpcRepositoryPath, '{{nameClean}}', CreateModule.nameClean)
+        await Files.replaceInFile(grpcRepositoryPath, '{{nameCamel}}', CreateModule.nameCamel)
 
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/repositories/drizzle.ts'))
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/repositories/memory.ts'))
+        await Files.deleteFile(drizzleRepositoryPath)
+        await Files.deleteFile(memoryRepositoryPath)
         break;
       default:
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/repositories/memory.ts'))
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/repositories/drizzle.ts'))
-        await Files.deleteFile(join(CreateModule.modulePath, 'infrastructure/repositories/grpc.ts'))
+        await Files.deleteFile(memoryRepositoryPath)
+        await Files.deleteFile(drizzleRepositoryPath)
+        await Files.deleteFile(grpcRepositoryPath)
         break;
     }
   }
